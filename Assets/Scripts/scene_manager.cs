d08c9b45baa74897ce73092e72b3455f1daafb4b
Tom Chang <tom0chang1516@gmail.com>
2017-05-30 14:07:20 +1200

    Menu completed

---
+++ Assets/Scripts/scene_manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_manager : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void start_game () {
+        SceneManager.LoadScene("test");
+	}
+
+    public void end()
+    {
+        Application.Quit();
+    }
+}
