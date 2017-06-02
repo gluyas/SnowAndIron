b4b1de21ed0d47ed446bd58929e462e016e040c1
Kun Huang <huangkun101230@gmail.com>
2017-06-02 16:19:47 +1200

    two placer works now

--- Assets/Scripts/UI/UnitPlacer2.cs
+++ Assets/Scripts/UI/UnitPlacer2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Model;
 using UnityEngine.UI;
 
-public class UnitPlacer1 : MonoBehaviour {
+public class UnitPlacer2 : MonoBehaviour {
 
 	public GameObject[] Units;
 	public GameController gameController;
@@ -20,9 +20,9 @@ public class UnitPlacer1 : MonoBehaviour {
 	public GUISkin customSkin1;
 
 	private int currentPlayer = 0;
-	
+
 	Transform _t;
-	TileVector _pos = new TileVector(0,0);
+	TileVector _pos = new TileVector(0,5);
 	CardinalDirection _facingUp = CardinalDirection.Northeast;
 	CardinalDirection _facingDown = CardinalDirection.Southeast;
 
@@ -31,44 +31,44 @@ public class UnitPlacer1 : MonoBehaviour {
 	void Start () {
 		_t = GetComponent<Transform> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 		if (lockInHex == false) {
-			if (Input.GetKeyDown (KeyCode.W))
+			if (Input.GetKeyDown (KeyCode.O))
 				MovePos (CardinalDirection.North);
-			if (Input.GetKeyDown (KeyCode.S))
+			if (Input.GetKeyDown (KeyCode.L))
 				MovePos (CardinalDirection.South);
-			if (Input.GetKeyDown (KeyCode.A))
+			if (Input.GetKeyDown (KeyCode.K))
 				MovePos (CardinalDirection.Southwest);
-			if (Input.GetKeyDown (KeyCode.D))
+			if (Input.GetKeyDown (KeyCode.Semicolon))
 				MovePos (CardinalDirection.Southeast);
-			if (Input.GetKeyDown (KeyCode.Q))
+			if (Input.GetKeyDown (KeyCode.I))
 				MovePos (CardinalDirection.Northwest);
-			if (Input.GetKeyDown (KeyCode.E))
+			if (Input.GetKeyDown (KeyCode.P))
 				MovePos (CardinalDirection.Northeast);
 		}
 
-		if (Input.GetKeyDown(KeyCode.V) || Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.B)) {
+		if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
 			lockInHex = !lockInHex;
 		}
 
 		for (int i = 0; i < Units.Length; i++) {
-			if (Input.GetKeyDown (KeyCode.V)) {
+			if (Input.GetKeyDown (KeyCode.DownArrow)) {
 				mid_l = true;
 			} 
-			if (Input.GetKeyDown (KeyCode.C)) {
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 				light_l = true;
 			} 
-			if (Input.GetKeyDown (KeyCode.B)) {
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
 				heavy_l = true;
-			} else if (Input.GetKeyDown (KeyCode.F)) {
+			} else if (Input.GetKeyDown (KeyCode.M)) {
 				lockUp = true;
-			} else if (Input.GetKeyDown (KeyCode.G)) {
+			} else if (Input.GetKeyDown (KeyCode.Comma)) {
 				lockDown = true;
 			}
-			 else if (lockInHex == true && mid_l == true && lockUp == true) {
+			else if (lockInHex == true && mid_l == true && lockUp == true) {
 				PlaceMech (Units [1], _facingUp);
 			}
 			else if (lockInHex == true && mid_l == true && lockDown == true) {
@@ -86,23 +86,23 @@ public class UnitPlacer1 : MonoBehaviour {
 			else if (lockInHex == true && heavy_l == true && lockDown == true) {
 				PlaceMech (Units [2], _facingDown);
 			}
-		
+
 		}
 
 	}
 
-//	void OnGUI(){
-//		GUI.skin = customSkin1;
-////		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 20, 120, 30), "Start")) {
-////			Debug.Log ("Start button is pressed");
-////		}
-//		for (int i = 0; i < Units.Length; i++) {
-//			if (lockInHex == true && GUI.Button (new Rect (Screen.width / 20, Screen.height / 20 + Screen.height / 8.5f * i, 100, 25), Units [i].name)) {
-//					PlaceMech (Units[i]);
-//			}
-//
-//		}
-//	}
+	//	void OnGUI(){
+	//		GUI.skin = customSkin1;
+	////		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 20, 120, 30), "Start")) {
+	////			Debug.Log ("Start button is pressed");
+	////		}
+	//		for (int i = 0; i < Units.Length; i++) {
+	//			if (lockInHex == true && GUI.Button (new Rect (Screen.width / 20, Screen.height / 20 + Screen.height / 8.5f * i, 100, 25), Units [i].name)) {
+	//					PlaceMech (Units[i]);
+	//			}
+	//
+	//		}
+	//	}
 
 	void MovePos(CardinalDirection direction) {
 		_pos = _pos + direction;
@@ -118,14 +118,14 @@ public class UnitPlacer1 : MonoBehaviour {
 		mid_l = false;			//2
 		heavy_l = false;		//2
 	}
-		
+
 
 	public void PlaceMech(GameObject GameUnit, CardinalDirection _facing)
 	{
 		Reset ();
-		if (gameController.MakeUnit(GameUnit, _pos, _facing, gameController.Players[0]))
+		if (gameController.MakeUnit(GameUnit, _pos, _facing, gameController.Players[1]))
 		{
-//			currentPlayer = (currentPlayer + 1) % gameController.Players.Length;
+			//			currentPlayer = (currentPlayer + 1) % gameController.Players.Length;
 			if (currentPlayer == 0) gameController.DoTurn();
 		}
 	}
