56a4cf607d00c2bd0d0583520214a78e3f17fe62
Kun Huang <huangkun101230@gmail.com>
2017-06-24 23:54:51 +1200

    added round timer

---
+++ Assets/timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class timer : MonoBehaviour {
+	
+	public GameController gameController;
+	public int RoundNumber;
+	public Image bar;
+	private int round; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		round = gameController.RoundNumber;
+		bar.fillAmount = (float)round / RoundNumber;
+	}
+}
