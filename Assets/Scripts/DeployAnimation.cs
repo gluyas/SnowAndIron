32b702bebe7d0b2af1d2dac1a202d55f80541370
Marc <marc.gluyas@gmail.com>
2017-06-28 13:32:42 +1200

    Added base implementation of DeployAnimation

--- Assets/Scripts/DeployAnimation.cs
+++ Assets/Scripts/DeployAnimation.cs
@@ -1,14 +1,60 @@
 ﻿using Model;
+using UnityEngine;
 
 public class DeployAnimation : IAnimation
 {
-	public DeployAnimation(Unit unit)
+	public Vector3 ProjectileOrigin
 	{
-		
+		get { return GuiComponents.GetDeployProjectileOrigin(); }
+	}
+
+	private GameObject _projectile;
+	private bool _impactTriggered;
+	private float _elapsed;
+	
+	private UnitAvatar _unit;
+	
+	public DeployAnimation(UnitAvatar unit)
+	{
+		_unit = unit;
 	}
 	
 	public bool ApplyAnimation(float time)
 	{
-		throw new System.NotImplementedException();
+		if (!_impactTriggered && _projectile == null)
+		{
+			_unit.Scale = 0;
+			_projectile = Object.Instantiate(GuiComponents.GetDeployProjectile());
+			_projectile.transform.position = ProjectileOrigin + GuiComponents.GetEffectHeightOffset();
+		}
+
+		if (!_impactTriggered)	// first stage : entrace time
+		{
+			_projectile.transform.position = Vector3.Lerp(ProjectileOrigin, _unit.Position, _elapsed);
+			_elapsed += time / GuiComponents.GetDeployEntranceTime();
+			
+			if (_elapsed >= 1)	// set up for second stage
+			{
+				var smoke = Object.Instantiate(GuiComponents.GetDeployEffect());
+				smoke.transform.position = _unit.Position;
+				
+				Object.Destroy(_projectile);
+				
+				_impactTriggered = true;
+				_elapsed = 0;
+			}
+		}
+		else 					// second stage : unit scale time
+		{
+			_unit.Scale = _elapsed;
+			_elapsed += time / GuiComponents.GetDeplotExitTime();
+			
+			if (_elapsed >= 1)
+			{
+				_unit.Scale = 1;
+				return true;
+			}
+		}
+		return false;
 	}
 }
